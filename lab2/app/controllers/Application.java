14c69b44a67f7b53d21c39d71797e43165d3bf8a
allysonss <allyson.de.soares@ccc.ufcg.edu.br>
2013-11-26 13:20:51 -0300

    Metodo de adicionar tarefas adicionado

--- lab2/app/controllers/Application.java
+++ lab2/app/controllers/Application.java
@@ -6,27 +6,35 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-	
+
 	static Form<Task> taskForm = Form.form(Task.class);
 
-    public static Result index() {
-    	return redirect(routes.Application.newTask());
-    }
-    
-    public static Result helloWorld() {
-    	return ok("Hello World!");
-    }
-    
-    public static Result tasks() {
-    	return ok(views.html.index.render(Task.all(), taskForm));
-    }
-    
-    public static Result newTask() {
-    	return TODO;
-    }
-    
-    public static Result deleteTask(Long id) {
-    	return TODO;
-    }
+	public static Result index() {
+		return redirect(routes.Application.newTask());
+	}
+
+	public static Result helloWorld() {
+		return ok("Hello World!");
+	}
+
+	public static Result tasks() {
+		return ok(views.html.index.render(Task.all(), taskForm));
+	}
+
+	public static Result newTask() {
+		  Form<Task> filledForm = taskForm.bindFromRequest();
+		  if(filledForm.hasErrors()) {
+		    return badRequest(
+		      views.html.index.render(Task.all(), filledForm)
+		    );
+		  } else {
+		    Task.create(filledForm.get());
+		    return redirect(routes.Application.tasks());  
+		  }
+		}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
 
 }
